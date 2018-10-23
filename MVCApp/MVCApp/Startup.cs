7655da73d58679cfe26853564cd5f4eee19ad517
inkh <jhwe92@gmail.com>
2018-10-22 21:34:49 -0700

    Added default pipeline and MVC folder structure

--- MVCApp/MVCApp/Startup.cs
+++ MVCApp/MVCApp/Startup.cs
@@ -15,6 +15,7 @@ namespace MVCApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,6 +29,17 @@ namespace MVCApp
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
+            });
+
+            //Allows use for js and css files
+            app.UseStaticFiles();
+
+            //Default route pipeline
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
