2870b2cb343fb535a891c6f2055c82cc7263c616
inkh <jhwe92@gmail.com>
2018-10-23 22:16:21 -0700

    added results view, populated results view with people data

--- MVCApp/MVCApp/Controllers/HomeController.cs
+++ MVCApp/MVCApp/Controllers/HomeController.cs
@@ -3,14 +3,44 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MVCApp.Models;
 
 namespace MVCApp.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Our default route at '/'
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Post request that captures form data
+        /// </summary>
+        /// <param name="begYear">Form input year beginning</param>
+        /// <param name="endYear">Form input year end</param>
+        /// <returns>Result view</returns>
+        [HttpPost]
+        public IActionResult Index(int begYear, int endYear)
+        {
+            return RedirectToAction("Result", new { begYear, endYear});
+        }
+
+        /// <summary>
+        /// Renders result page with current data.
+        /// </summary>
+        /// <param name="begYear">Form input year beginning</param>
+        /// <param name="endYear">Form input year end</param>
+        /// <returns></returns>
+        public IActionResult Result(int begYear, int endYear)
+        {
+            List<PersonOfTheYear> myList = PersonOfTheYear.GetPersons(begYear, endYear);
+            return View(myList);
         }
     }
 }
--- MVCApp/MVCApp/Models/PersonOfTheYear.cs
+++ MVCApp/MVCApp/Models/PersonOfTheYear.cs
@@ -8,6 +8,7 @@ namespace MVCApp.Models
 {
     public class PersonOfTheYear
     {
+        //Properties of each PersonOfTheYear
         public int Year { get; set; }
         public string Honor { get; set; }
         public string Name { get; set; }
@@ -18,6 +19,12 @@ namespace MVCApp.Models
         public string Category { get; set; }
         public string Context { get; set; }
 
+        /// <summary>
+        /// Gets the list of people within begYear and endYear
+        /// </summary>
+        /// <param name="begYear">Beginning Year</param>
+        /// <param name="endYear">Ending Year</param>
+        /// <returns>List of PersonOfTheYear objects within begYear and endYear</returns>
         public static List<PersonOfTheYear> GetPersons(int begYear, int endYear)
         {
             //Creates a collection of PersonOfTheYear instances to be populated by data
@@ -32,11 +39,13 @@ namespace MVCApp.Models
             //Reads through the file and stores every line(data) into an array
             string[] myData = File.ReadAllLines(newPath);
 
-            Console.WriteLine(myData);
-
+            //Loops through the read data and skips the first line
             for (int i = 1; i < myData.Length; i++)
             {
+                //Separate each individual line by commas into an array.
                 string[] fields = myData[i].Split(',');
+
+                //Add new instance of PersonOfTheYear to people list with corresponding data.
                 people.Add(new PersonOfTheYear
                 {
                     Year = Convert.ToInt32(fields[0]),
@@ -50,7 +59,11 @@ namespace MVCApp.Models
                     Context = fields[8]
                 });
             }
+
+            //Filters the list of people that fits within the begYear - endYear criteria
             List<PersonOfTheYear> listOfPeople = people.Where(p => (p.Year >= begYear) && (p.Year <= endYear)).ToList();
+
+            //Returns the filtered list
             return listOfPeople;
         }
     }
